7dd13600030fed3fae80a0cd82beab6c3a441b73
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2023-05-10 20:54:32 +0200

    Mejorando el menu

--- src/main/java/interfaces/MainMenuWindow.java
+++ src/main/java/interfaces/MainMenuWindow.java
@@ -1,16 +1,10 @@
 package interfaces;
 
-import javafx.embed.swing.JFXPanel;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
-import javafx.util.Duration;
-
 import javax.swing.*;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.Color;
 
 public class MainMenuWindow extends JPanel {
@@ -27,29 +21,15 @@ public class MainMenuWindow extends JPanel {
         titleMainMenu.setFont(new Font("HYWenHei-85W", Font.PLAIN, 80));
         titleMainMenu.setBounds(399, 11, 638, 138);
         add(titleMainMenu);
-
-        // Se crea un objeto JFXPanel para contener el video
-        final JFXPanel fxPanel = new JFXPanel();
-        fxPanel.setBounds(-95, -25, 1459, 774);
-        add(fxPanel);
-
-        // Se crea un objeto Media que contiene la URL del video
-        Media media = new Media("C:\\Users\\1DAW\\Desktop\\GitHub\\Eternal-Ashes\\videos\\mainmenuvideo.mp4");
-
-        // Se crea un objeto MediaPlayer y se lo configura para reproducirse en bucle
-        MediaPlayer mediaPlayer = new MediaPlayer(media);
-        mediaPlayer.setOnEndOfMedia(() -> {
-            mediaPlayer.seek(Duration.ZERO);
-        });
-        mediaPlayer.setAutoPlay(true);
-
-        // Se crea un objeto MediaView y se lo agrega al JFXPanel
-        MediaView mediaView = new MediaView(mediaPlayer);
-        Group root = new Group();
-        root.getChildren().add(mediaView);
-        Scene scene = new Scene(root);
-        fxPanel.setScene(scene);
-
+        
+        JLabel backgroundMainMenu = new JLabel("");
+		backgroundMainMenu.setHorizontalTextPosition(SwingConstants.CENTER);
+		backgroundMainMenu.setHorizontalAlignment(SwingConstants.CENTER);
+		backgroundMainMenu.setIcon(new ImageIcon("C:\\Users\\1DAW\\Desktop\\GitHub\\Eternal-Ashes\\images\\mainwallpaper.jpg"));
+		backgroundMainMenu.setMaximumSize(new Dimension(1920, 1080));
+		backgroundMainMenu.setSize(new Dimension(1920, 1080));
+		backgroundMainMenu.setBounds(-95, -25, 1459, 774);
+        
         JLabel closeButton = new JLabel("");
         closeButton.setBackground(new Color(255, 128, 128));
         closeButton.setHorizontalAlignment(SwingConstants.CENTER);
@@ -67,7 +47,29 @@ public class MainMenuWindow extends JPanel {
                 closeButton.setIcon(new ImageIcon("C:\\Users\\1DAW\\Desktop\\GitHub\\Eternal-Ashes\\images\\botonMainMenuCerrar.png"));
             }
         });
+        
+        JLabel labelStart = new JLabel("Clic para empezar");
+        labelStart.setFont(new Font("HYWenHei-85W", Font.PLAIN, 20));
+        labelStart.setForeground(new Color(255, 255, 255));
+        labelStart.setBounds(610, 442, 403, 68);
+        Timer timer = new Timer(20, new ActionListener() {
+            private int opacity = 255;
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                opacity -= 5;
+                labelStart.setForeground(new Color(255, 255, 255, opacity));
+                if (opacity <= 0) {
+                    ((Timer) e.getSource()).stop();
+                }
+            }
+        });
+        timer.setRepeats(true);
+        timer.start();
+        add(labelStart);
         add(closeButton);
+        add(backgroundMainMenu);
+        
     }
+    
 }
 
