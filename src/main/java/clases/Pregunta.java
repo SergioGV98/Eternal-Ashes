1a7aa54316160766d8ead504168b24ce70bb456e
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2023-05-19 14:21:10 +0200

    Clase pregunta funcionable
    La clase pregunta genera una pregunta aleatoria de la categoria que se eliga.

--- src/main/java/clases/Pregunta.java
+++ src/main/java/clases/Pregunta.java
@@ -7,34 +7,31 @@ import java.util.LinkedHashSet;
 import java.util.Random;
 
 import enums.TipoPregunta;
+import utils.Config;
 import utils.DAO;
 
 public class Pregunta {
 
 	private String enunciado;
 	private TipoPregunta generoPregunta;
-	private HashMap <String, Boolean> respuestas;
-	
+	private HashMap<String, Boolean> respuestas;
+
 	public Pregunta(TipoPregunta genero) throws SQLException {
-        this.respuestas = new HashMap<>();
+		this.respuestas = new HashMap<>();
 
-        ArrayList<Object> pregunta = obtenerPregunta(genero);
-        
-        this.enunciado = (String) pregunta.get(0);
-        
-        if(pregunta.get(1).equals("Anime")) {
-        	this.generoPregunta = TipoPregunta.ANIME;
-        }
-        
-        // Agregar las respuestas incorrectas al HashMap
-        this.respuestas.put((String) pregunta.get(2), false);
-        this.respuestas.put((String) pregunta.get(3), false);
-        this.respuestas.put((String) pregunta.get(4), false);
+		ArrayList<Object> pregunta = obtenerPregunta(genero);
 
-        // Agregar la respuesta correcta al HashMap
-        this.respuestas.put((String) pregunta.get(5), true);
-    }
-	
+		this.enunciado = (String) pregunta.get(0);
+
+		if (pregunta.get(1).equals("Anime")) {
+			this.generoPregunta = TipoPregunta.ANIME;
+		}
+		this.respuestas.put((String) pregunta.get(2), false);
+		this.respuestas.put((String) pregunta.get(3), false);
+		this.respuestas.put((String) pregunta.get(4), false);
+		this.respuestas.put((String) pregunta.get(5), true);
+	}
+
 	public String getEnunciado() {
 		return enunciado;
 	}
@@ -59,10 +56,10 @@ public class Pregunta {
 		this.respuestas = respuestas;
 	}
 
-	public ArrayList<Object> obtenerPregunta(TipoPregunta tipo) throws SQLException{
+	public ArrayList<Object> obtenerPregunta(TipoPregunta tipo) throws SQLException {
 		ArrayList<Object> pregunta;
 		HashMap<String, Object> restricciones = new HashMap<String, Object>();
-		if(tipo == TipoPregunta.ANIME) {
+		if (tipo == TipoPregunta.ANIME) {
 			restricciones.put("genero", "Anime");
 		}
 		LinkedHashSet<String> consulta = new LinkedHashSet<String>();
@@ -73,15 +70,19 @@ public class Pregunta {
 		consulta.add("respuestaincorrecta3");
 		consulta.add("respuestacorrecta");
 		pregunta = DAO.consultar("preguntas", restricciones, consulta);
-		Random r = new Random();
-		byte cuenta = 0;
-		for(short i = 0; i < pregunta.size(); i += 6) {
-			++cuenta;
-			System.out.println(cuenta);
+
+		if (!pregunta.isEmpty()) {
+			Random r = new Random();
+			int indiceAleatorio = r.nextInt(pregunta.size() / 6) * 6;
+			ArrayList<Object> preguntaAleatoria = new ArrayList<>(
+					pregunta.subList(indiceAleatorio, indiceAleatorio + 6));
+			if(Config.verboseMode) {
+				System.out.println(preguntaAleatoria);
+			}
+			return preguntaAleatoria;
+		} else {
+			return null; 
 		}
-		r.nextInt(0, pregunta.size());
-		System.out.println(pregunta.size());
-		return pregunta;
 	}
-	
+
 }
